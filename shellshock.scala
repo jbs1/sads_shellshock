b4c74283ac1664af88de671dc1ba4687c5149f23
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-03-02 18:27:33 +0100

    bla

--- shellshock.scala
+++ shellshock.scala
@@ -1,10 +1,14 @@
 object Console {
 	type NAME = String;
 
-	abstrac class EXPR()
-	case class variable() extends EXPR {
-		
-	}
+	sealed abstract class EXPR
+	case class variable(name: NAME) extends EXPR
+	case class string(str: String) extends EXPR
+
+	sealed abstract class COMM()
+	case class com_seq(coms: Array[COMM]) extends COMM
+	case class fun_call()
+
 
 	def main(args: Array[String]): Unit = {
 
